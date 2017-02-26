5f3daae45ece28af12e76dc5ee4adebe35212c4a
NilsG-S <NilsG-S@users.noreply.github.com>
2017-02-25 23:53:16 -0600

    initial commit

---
+++ spec/indenter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IndenterView from '../lib/indenter-view';
+
+describe('IndenterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
